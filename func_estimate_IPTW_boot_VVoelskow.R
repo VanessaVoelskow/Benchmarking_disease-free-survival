3e1c57ad87db0d1cebb56bfbf357eb700c5e690c
Vanessa Voelskow <93942249+VanessaVoelskow@users.noreply.github.com>
2025-02-06 15:47:15 +0100

    Updated func_estimate_IPTW_boot_VVoelskow.R

--- func_estimate_IPTW_boot_VVoelskow.R
+++ func_estimate_IPTW_boot_VVoelskow.R
@@ -7,24 +7,21 @@ estimate.stabilized.weights.99.boot <- function(df.surv, df, C, K, Z, p.denom, p
 
   ###     KEEP TIMES WHEN TRASTUZUMAB INITIATION WAS POSSIBLE
   
-  df.weight.estimation <- df.surv %>% 
-    filter(.data[[cum.treat.lag.col]] == C, time >= K, time <= Z)
+  df.weight.estimation <- df.surv[get(cum.treat.lag.col) == C & time >= K & time <= Z]
   
   ###     WEIGHTING - DENOMINATOR
   
   m.denom <- glm(p.denom, family = binomial(link = "logit"), data = df.weight.estimation)
-  df.surv <- df.surv %>% 
-    mutate(m.denom = predict(m.denom, ., type = "response"),
-           m.denom.t = ifelse(.data[[treat.col]] == 1, m.denom, 1 - m.denom),
-           m.denom.t = ifelse(.data[[cum.treat.lag.col]] == C & time >= K & time <= Z, m.denom.t, 1))
+  df.surv$m.denom <- predict(m.denom, df.surv, type = "response")
+  df.surv[, m.denom.t := fifelse(get(treat.col) == 1, m.denom, 1-m.denom)]
+  df.surv[, m.denom.t := fifelse(get(cum.treat.lag.col) == C & time >= K & time <= Z, m.denom.t, 1)]
   
   ###     WEIGHTING - NUMERATOR
   
   m.num <- glm(p.num, family = binomial(link = "logit"), data = df.weight.estimation)
-  df.surv <- df.surv %>% 
-    mutate(m.num = predict(m.num, ., type = "response"),
-           m.num.t = ifelse(.data[[treat.col]] == 1, m.num, 1 - m.num),
-           m.num.t = ifelse(.data[[cum.treat.lag.col]] == C & time >= K & time <= Z, m.num.t, 1))
+  df.surv$m.num <- predict(m.num, df.surv, type = "response")
+  df.surv[, m.num.t := fifelse(get(treat.col) == 1, m.num, 1-m.num)]
+  df.surv[, m.num.t := fifelse(get(cum.treat.lag.col) == C & time >= K & time <= Z, m.num.t, 1)]
   
   ###     ESTIMATE FINAL WEIGHTS
   
@@ -33,15 +30,14 @@ estimate.stabilized.weights.99.boot <- function(df.surv, df, C, K, Z, p.denom, p
     mutate(sw = cumprod(m.num.t) / cumprod(m.denom.t)) %>%
     ungroup()
   
-  df.sw.d <- df.surv %>% 
-    select(blopnr, time, sw) %>%
-    inner_join(df, by = c("blopnr", "time"))
+  d.sw <- df.surv[, c("blopnr", "time", "sw")]
+  df.sw.d <- merge(d.sw, df, by = c("blopnr", "time"))
   
   ###     TRUNCATION OF WEIGHTS
 
   t.99 <- quantile(df.sw.d$sw, 0.99, na.rm = TRUE)
-  df.sw.d <- df.sw.d %>%
-    mutate(sw.99.boot = pmin(sw, t.99))
+  df.sw.d$sw.99.boot <- df.sw.d$sw
+  df.sw.d$sw.99.boot[df.sw.d$sw.99.boot > t.99] <- t.99
   
   assign("df.sw.d", df.sw.d, envir = .GlobalEnv)
   assign("sw.99.boot", df.sw.d$sw.99.boot, envir = .GlobalEnv)
